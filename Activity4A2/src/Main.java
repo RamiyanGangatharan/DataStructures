230ab91d4c996313201d8411db2b24c47de7660a
Ramiyan Gangatharan <ramiyan2185@gmail.com>
2025-09-24 09:30:11 -0400

    yes

--- Activity4A2/src/Main.java
+++ Activity4A2/src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Stack;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main (String[] args)
+    {
+        Stack<Integer> intStack = new Stack<>();
+        Stack<String> strStack = new Stack<>();
+
+        intStack.push(2001);
+        intStack.push(2002);
+        intStack.push(2003);
+        intStack.push(2004);
+        intStack.push(2005);
+
+        strStack.push("lamb");
+        strStack.push("little");
+        strStack.push("a");
+        strStack.push("had");
+        strStack.push("Mary");
+
     }
 }
