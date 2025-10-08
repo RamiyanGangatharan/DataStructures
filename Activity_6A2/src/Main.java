01b43baeb91942e9fe3c1d60ad7c0699fe49f058
Ramiyan Gangatharan <ramiyan2185@gmail.com>
2025-10-08 10:10:41 -0400

    more changes

--- Activity_6A2/src/Main.java
+++ Activity_6A2/src/Main.java
@@ -1,38 +1,21 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        int[] nums = new int[]{1, 2, 3};
-        List<List<Integer>> results = generateSubsets(nums);
-        System.out.println("The subsets of " + Arrays.toString(nums) + " is: " + results);
+        int[] nums = {1, 2, 3};
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(nums, 0, new ArrayList<>(), result);
+        System.out.println(result);
     }
 
-    /**
-     * Generates all possible subsets (the power set) of a given integer array.
-     * Each subset is built by iteratively adding the current number to existing subsets.
-     * Mathematically, for an array of length n, there are 2^n possible subsets.
-     * This method starts with the empty subset and, for each number in the array,
-     * creates new subsets by adding that number to every existing subset.
-     *
-     * @param nums an array of integers
-     * @return a list containing all possible subsets of the input array
-     */
-    public static List<List<Integer>> generateSubsets(int[] nums) {
-        List<List<Integer>> subsets = new ArrayList<>();
-        subsets.add(new ArrayList<>()); // empty set
+    static void backtrack(int[] nums, int index, List<Integer> current, List<List<Integer>> result) {
+        // BASE
+        if (index == nums.length) { result.add(new ArrayList<>(current)); return; }
 
-        for (int num : nums) {
-            List<List<Integer>> newSubsets = new ArrayList<>();
-
-            for (List<Integer> subset : subsets) {
-                List<Integer> newSubset = new ArrayList<>(subset);
-                newSubset.add(num);
-                newSubsets.add(newSubset);
-            }
-            subsets.addAll(newSubsets);
-        }
-        return subsets;
+        backtrack(nums, index + 1, current, result);
+        current.add(nums[index]);
+        backtrack(nums, index + 1, current, result);
+        current.remove(current.size() - 1);
     }
 }
